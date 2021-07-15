0c5d89a3f58c1e68f43ffc7add95bf812f9393cd
LiamMaclean216 <lmaclean234@gmaill.com>
2021-07-15 16:27:18 -0400

    ranking

--- Script_python/Model_comparisons.py
+++ Script_python/Model_comparisons.py
@@ -120,34 +120,73 @@ def comparisons_model(img_size, weights=None):
     classification_model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=1e-5), metrics=['accuracy'])
     return classification_model
 
+def ranking_model(img_size, weights=None):
+    """
+    Create comparisons network which reproduce the choice in an images duel.
 
-if __name__ == "__main__":
-    # ------------------------------------------------------------------------------------------------------------------
-    # Variables initialization
-    # ------------------------------------------------------------------------------------------------------------------
-    width = 224
-    save_folder = r"D:\Guillaume\Ottawa\Data\Comparisons_npy\09_04"
-    data_left = np.load(os.path.join(save_folder, "train", "train_left_224.npy"))
-    data_right = np.load(os.path.join(save_folder, "train", "train_right_224.npy"))
-    data_label = np.load(os.path.join(save_folder, "train", "train_labels_224.npy"))
-    # ------------------------------------------------------------------------------------------------------------------
-    # Run functions
-    # ------------------------------------------------------------------------------------------------------------------
-    # Build npy
-    # trainLeft, trainRight, train_label = preprocessing_duels(csv_path, width, height, img_dir, save_folder, 0.1)
+    :param img_size: size of input images during training
+    :type img_size: tuple(int)
+    :param weights: path to the weights use for initialization
+    :type weights: str
+    :return: ranking comparisons model
+    :rtype: keras.Model
+    """
+    
+    #vgg_feature_extractor = VGG19(weights='imagenet', include_top=False, input_shape=(img_size, img_size, 3))
+    
+    #vgg_include_until='block4_pool'
+    #feature_extractor = VGG19(weights='imagenet', include_top=False, input_shape=(img_size, img_size, 3))
+    #feature_extractor = Model(inputs=vgg_feature_extractor.input, outputs=feature_extractor.get_layer(vgg_include_until).output)
+    
+    feature_extractor = VGG19(weights='imagenet', include_top=False, input_shape=(img_size, img_size, 3))
+    
+    
+    #feature_extractor = ResNet152V2(weights='imagenet', include_top=False, input_shape=(img_size, img_size, 3))
+    # Fine tuning by freezing the last 4 convolutional layers of VGG19 (last block)
+    #for layer in feature_extractor.layers[:-8]:
+    #    layer.trainable = False
+    #feature_extractor.trainable = False
 
-    # Shuffling data
-    data_left_shuffled, data_right_shuffled, data_label_shuffled = shuffle_unison_arrays([data_left, data_right, data_label])
+    # Definition of the 2 inputs
+    img = Input(shape=(224*224*3), name="data")
+    x = Reshape((224,224,3), input_shape=(224*224*3,))(img)
+    x = preprocessing_layers(x)
+    
+    
+    x = feature_extractor(x)
+   # x = cbam_block(x)
+    # Add convolution layers on top
+    x = BatchNormalization()(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name="Conv_1")(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.75, name="Drop_1")(x)
+    
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name="Conv_2")(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.75, name="Drop_2")(x)
+    x = Flatten()(x)
+    #Dropout(0.75, name="Drop_1")(x)
+    #x = Dense(4096, activation='relu', kernel_initializer='glorot_normal', name="Dense1")(x)
+    
+    #x = AveragePooling2D(pool_size=(7, 7))
+    #x = Flatten()(x)
+    
+    #x = Dense(512, activation='relu', kernel_initializer='glorot_normal', name="Dense1")(x)
+    #Dropout(0.5, name="Drop_1")(x)
+    
+    x = Dense(256, activation='relu', kernel_initializer='glorot_normal', name="Dense2")(x)
+    Dropout(0.75, name="Drop_2")(x)
+    
+    #Dropout(0.75, name="Drop_3")(x)
+    #x = Dense(1000, activation='relu', kernel_initializer='glorot_normal', name="Dense1")(x)
+    
+    x = Dense(1, activation=None, name="Final_dense")(x)
+    classification_model = Model(img, x)
 
-    # Simple training
-    folder_path = r"D:\Guillaume\Ottawa\Data\Training_Models_Results\Comparisons_Trueskill/Simple_retrain"
-    simple_training(data_left_shuffled, data_right_shuffled, data_label_shuffled, comparisons_model, [width], folder_path, val_split=0.2, epochs=300, batch_size=32)
+    if weights:
+        classification_model.load_weights(weights)
 
-    # K fold training
-    # folder_path = r"D:\Guillaume\Ottawa\Data\All_Results_to_sort\kfold-drop-out_normal"
-    # k_fold(data_left, data_right, data_label, 5, comparisons_model, [width], folder_path)
-
-    # Evaluation on test data
-    # save_folder = "D:/Guillaume/Ottawa/Script_python/Results/npy_13_08/"
-    # model_path = os.path.join(save_folder, "k-fold", "fitted_comparisons_k5_clear_session.h5")
-    # evaluation_test_set(model_path, save_folder, mode="comparisons")
+    #sgd = SGD(lr=1e-5, decay=1e-6, momentum=0.695, nesterov=True)
+    
+    classification_model.compile(loss='mse', optimizer=Adam(learning_rate=1e-5), metrics=['mae'])
+    return classification_model
